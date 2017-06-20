6c8618a702e89671f99a935aa510dd93aa891c7e
HecFranco <hector.franco.aceituno@gmail.com>
2017-06-20 11:55:56 +0200

    Create dates.php

---
+++ PHP/funciones/dates.php
@@ -0,0 +1,124 @@
+<?php
+//DATOS DE EJEMPLO
+$datos=
+ 	[
+ 		0=>
+ 		[
+ 				'id'			=>	13,
+ 				'date'			=>	'19-06-2017',
+ 				'nombre'		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			],
+ 		1=>
+ 			[
+ 				'id'			=>	84,
+ 				'date'			=>	'22-12-2012',
+ 				'nombre'		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			],
+ 		2=>
+ 			[
+ 				'id'			=>	75,
+ 				'date'			=>	'25-01-2013',
+ 				'nombre' 		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			],
+ 		3=>
+ 			[
+ 				'id'			=>	75,
+ 				'date'			=>	'25-01-2013',
+ 				'nombre' 		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			]
+ 	];
+  
+/* FUNCIÓN MUESTRA INFORMACIÓN DE UNA FECHA */
+
+function dateAdaptedType($date, $type){
+	switch($type){
+		case 'year':
+			// año de la fecha
+			$dateAdapted = date('Y', strtotime($date));
+		break;
+		case 'monthN':
+			// mes en número de la fecha
+			$dateAdapted = date('m', strtotime($date));
+		break;
+		case 'monthL':
+			// mes en número de la fecha
+			$dateAdapted = date('F', strtotime($date));
+		break;
+		case 'totalDayOfMonth':
+			// total de días del mes de la fecha
+			$dateAdapted = date('t', strtotime($date));
+		break;
+		case 'weeksNumbersMonthF':
+			// número de semanas que tiene el mes (fracciones)
+			$totalDayOfMonth = date('t', strtotime($date));		
+			$dateAdapted = $totalDayOfMonth/7;
+		break;
+		case 'weeksNumbersMonthT':
+			// número de semanas que tiene el mes (entera)
+			$totalDayOfMonth = date('t', strtotime($date));		
+			$weeksNumbersMonthF = $totalDayOfMonth/7;
+			$dateAdapted = round($weeksNumbersMonthF);
+		break;
+		case 'weeksNumbersMonthR':
+			// días sueltos semanas mes
+			$totalDayOfMonth = date('t', strtotime($date));		
+			$weeksNumbersMonthF = $totalDayOfMonth/7;
+			$weeksNumbersMonthT = round($weeksNumbersMonthF);
+			$dateAdapted = ($weeksNumbersMonthF - $weeksNumbersMonthT) * 7;
+		break;
+		case 'dayOfMonth':		
+			// día del mes de la fecha
+			$dateAdapted = date('d', strtotime($date));
+		break;
+		case 'fristDayOfWeekMonthL':
+			// primer día del mes en semana numero de la fecha
+			$year = date('Y', strtotime($date));
+			$monthN = date('m', strtotime($date));
+			$dateAdapted = date("l", mktime(0,0,0,$monthN,1,$year));
+		break;
+		case 'fristDayOfWeekMonthN':
+			// primer día del mes en semana numero de la fecha
+			$year = date('Y', strtotime($date));
+			$monthN = date('m', strtotime($date));
+			$dateAdapted = date("w", mktime(0,0,0,$monthN,1,$year));
+		break;
+		case 'daysRemain';
+			// días que quedan del mes de la fecha		
+			$totalDayOfMonth = date('t', strtotime($date));
+			$dayOfMonth = date('d', strtotime($date));
+			$dateAdapted = $totalDayOfMonth - $dayOfMonth;
+		break;
+		case 'dayOfWeekL';
+			// día de la semana en letra de la fecha
+			$dateAdapted = date('l', strtotime($datos[$clave]['date']));
+		}
+	return $dateAdapted;
+}
+
+$year=dateAdaptedType('25-01-2013','year');
+
+echo $year."<br>";
+
+/* FUNCIÓN GENERA UN ARRAY CON DATOS DE FECHAS */
+
+function datesArray ($datos){
+	$length=count($datos)-1;
+	for( $clave=0 ; $clave<=$length ; $clave++ ){
+		// fecha
+		$date = $datos[$clave]['date'];
+			$datesArray[$clave]['date'] = $date;
+		// año de la fecha
+		$datesArray[$clave]['year'] = dateAdaptedType($date, 'year');
+	};
+	return $datesArray;
+};
+
+var_dump(datesArray ($datos));
