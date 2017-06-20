c2b8fa6dd47ffc9b48a9a7a5ded4845d4fd03874
HecFranco <hector.franco.aceituno@gmail.com>
2017-06-20 15:28:48 +0200

    Create dataExample.php

---
+++ PHP/funciones/dataExample.php
@@ -0,0 +1,37 @@
+<?php
+//DATOS DE EJEMPLO
+$datos=
+ 	[
+ 		0=>
+ 		[
+ 				'id'			=>	13,
+ 				'date'			=>	'19-06-2017',
+ 				'nombre'		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			],
+ 		1=>
+ 			[
+ 				'id'			=>	84,
+ 				'date'			=>	'22-12-2012',
+ 				'nombre'		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			],
+ 		2=>
+ 			[
+ 				'id'			=>	75,
+ 				'date'			=>	'25-01-2013',
+ 				'nombre' 		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			],
+ 		3=>
+ 			[
+ 				'id'			=>	75,
+ 				'date'			=>	'25-01-2013',
+ 				'nombre' 		=>	'pepe',
+ 				'horaentrada'	=>	'8:50',
+ 				'horasalida'	=>	'14:35'
+ 			]
+ 	];
