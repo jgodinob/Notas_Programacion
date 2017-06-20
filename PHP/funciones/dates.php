43d7916a589aec9982105b265bde9891f30baa79
HecFranco <hector.franco.aceituno@gmail.com>
2017-06-20 15:28:19 +0200

    Update dates.php

--- PHP/funciones/dates.php
+++ PHP/funciones/dates.php
@@ -1,41 +1,5 @@
 <?php
-//DATOS DE EJEMPLO
-$datos=
- 	[
- 		0=>
- 		[
- 				'id'			=>	13,
- 				'date'			=>	'19-06-2017',
- 				'nombre'		=>	'pepe',
- 				'horaentrada'	=>	'8:50',
- 				'horasalida'	=>	'14:35'
- 			],
- 		1=>
- 			[
- 				'id'			=>	84,
- 				'date'			=>	'22-12-2012',
- 				'nombre'		=>	'pepe',
- 				'horaentrada'	=>	'8:50',
- 				'horasalida'	=>	'14:35'
- 			],
- 		2=>
- 			[
- 				'id'			=>	75,
- 				'date'			=>	'25-01-2013',
- 				'nombre' 		=>	'pepe',
- 				'horaentrada'	=>	'8:50',
- 				'horasalida'	=>	'14:35'
- 			],
- 		3=>
- 			[
- 				'id'			=>	75,
- 				'date'			=>	'25-01-2013',
- 				'nombre' 		=>	'pepe',
- 				'horaentrada'	=>	'8:50',
- 				'horasalida'	=>	'14:35'
- 			]
- 	];
-  
+
 /* FUNCIÓN MUESTRA INFORMACIÓN DE UNA FECHA */
 
 function dateAdaptedType($date, $type){
