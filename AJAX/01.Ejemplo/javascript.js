fa189622814c75d8f76cd49056362d321c171cbd
HecFranco <hector.franco.aceituno@gmail.com>
2017-08-10 14:54:42 +0200

    Create javascript.js

---
+++ AJAX/01.Ejemplo/javascript.js
@@ -0,0 +1,5 @@
+//cuando useda el evento click se va ejectuar y mostrara el archivo que cargamos
+ $('#boton-ajax').on('click', function(){
+        //seleccionamos donde se mostrara y el archivo que se cargara
+        $('#ajax-load').load('cargar.html');
+});
